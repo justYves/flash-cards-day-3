aa5775c2d3009fc3b522622bbb4fcc3d2c844cbe
Yves Yuen <yves.j.yuen@gmail.com>
2015-07-10 17:09:19 -0400

    Added directives for FlashCards

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,25 +1,13 @@
 app.controller('MainController', function($scope, FlashCardsFactory, ScoreFactory) {
-  $scope.clicked = "All";
 
+  $scope.clicked = "All";
   // $scope.flashCards = whateverName;
   // $scope.flashCards = FlashCardsFactory.getFlashCards();
   FlashCardsFactory.getFlashCards()
-    .then(function(data){
+    .then(function(data) {
       $scope.flashCards = data;
     });
 
-  $scope.answerQuestion = function(answer, flashCard) {
-    if (!flashCard.answered) {
-      flashCard.answered = true;
-      if (answer.correct) {
-        ++ScoreFactory.correct;
-      } else {
-        ++ScoreFactory.incorrect;
-      }
-
-      flashCard.answeredCorrectly = answer.correct;
-    }
-  };
   $scope.getCategoryCards = function(category) {
     $scope.clicked = category;
     FlashCardsFactory.getFlashCards(category)
---
+++ public/js/directives/flash-card/flash-card.js
@@ -0,0 +1,29 @@
+app.directive('flashCard',function(ScoreFactory){
+  return {
+    restrict: 'E',
+    scope: {
+      card: '='
+    },
+    link : function(scope, element, attribute, controller){
+      // controller.answerQuestion(scope.flashCard.answer);
+
+  scope.answerQuestion = function(answer) {
+    console.log(scope);
+    if (!scope.answered) {
+      scope.answered = true;
+      if (answer.correct) {
+        ++ScoreFactory.correct;
+      } else {
+        ++ScoreFactory.incorrect;
+      }
+      scope.answeredCorrectly = answer.correct;
+    }
+  };
+
+
+
+    },
+    templateUrl: 'js/directives/flash-card/flash-card.html'
+  };
+});
+
